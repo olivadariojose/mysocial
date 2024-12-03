2833731c022915879eba59621c19afdf1bc27657
olivadariojose <olivadariojose@gmail.com>
2024-12-02 22:44:24 -0600

    completed

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const fontSizes = document.querySelectorAll('.choose-size span')
 
 var root = document.querySelector(':root')
 
+const colorPalette = document.querySelectorAll('.choose-color span')
+
+const Bg1 = document.querySelector('.bg-1')
+const Bg2 = document.querySelector('.bg-2')
+const Bg3 = document.querySelector('.bg-3')
+
 // removemos la clase active de todos los items del menu
 const changeActiveItem = () => {
     menuItems.forEach(item => {
@@ -123,4 +129,81 @@ fontSizes.forEach(size=>{
         
         document.querySelector('html').style.fontSize=fontSize
     })
+})
+
+
+//  CHANGE PRIMARY COLORS
+const changeActiveColorClss = ()=>{
+    colorPalette.forEach(colorPicker=>{
+        colorPicker.classList.remove('active')
+    })
+}
+
+colorPalette.forEach(color=>{
+    color.addEventListener('click',()=>{
+        let primaryHue
+        changeActiveColorClss()
+        if(color.classList.contains('color-1')){
+            primaryHue=252
+            
+        } else if(color.classList.contains('color-2')){
+            primaryHue=52
+        } else if(color.classList.contains('color-3')){
+            primaryHue=352
+        } else if(color.classList.contains('color-4')){
+            primaryHue=152
+        } else if(color.classList.contains('color-5')){
+            primaryHue=202
+        }
+
+        color.classList.add('active')
+        root.style.setProperty('--primary-color-hue', primaryHue)
+    })
+})
+
+//  theme background
+
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+const changeBG=()=>{
+    root.style.setProperty('--light-color-lightness', lightColorLightness)
+    root.style.setProperty('--white-color-lightness', whiteColorLightness)
+    root.style.setProperty('--dark-color-lightness', darkColorLightness)
+}
+
+Bg1.addEventListener('click',()=>{
+
+    Bg1.classList.add('active')
+    Bg2.classList.remove('active')
+    Bg3.classList.remove('active')
+
+    window.location.reload()
+
+})
+
+
+Bg2.addEventListener('click',()=>{
+    darkColorLightness = '95%'
+    whiteColorLightness = '20%'
+    lightColorLightness = '15%'
+
+    Bg2.classList.add('active')
+    Bg1.classList.remove('active')
+    Bg3.classList.remove('active')
+
+    changeBG()
+})
+
+Bg3.addEventListener('click',()=>{
+    darkColorLightness = '95%'
+    whiteColorLightness = '10%'
+    lightColorLightness = '0%'
+
+    Bg3.classList.add('active')
+    Bg1.classList.remove('active')
+    Bg2.classList.remove('active')
+
+    changeBG()
 })
